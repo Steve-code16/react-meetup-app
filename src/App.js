4efbccfa3212e270c350ad8fc42da20553b35620
Steve Molele <stevemolele@yahoo.com>
2022-04-23 17:15:43 +0200

    added routes for the different pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import AllMeetupsPage from "./pages/AllMeetups";
+import FavoritesPage from "./pages/Favorites";
+import NewMeetupPage from "./pages/NewMeetup";
+
 function App() {
-  return <div>Let's Meet.</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<AllMeetupsPage />} />
+
+        <Route path="/favorites" element={<FavoritesPage />} />
+
+        <Route path="/new-meetup" element={<NewMeetupPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
