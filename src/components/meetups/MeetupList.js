675861c784f012b7f6b2a871483a39a47b8be3c1
Steve Molele <stevemolele@yahoo.com>
2022-04-25 00:53:44 +0200

    rendering a list of data to pages

---
+++ src/components/meetups/MeetupList.js
@@ -0,0 +1,7 @@
+import classes from "./MeetupList.module.css";
+
+function MeetupList(props) {
+  return;
+}
+
+export default MeetupList;
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,5 +1,44 @@
+const DUMMY_DATA = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Law_Chambers-011_perspective_corrected.jpg/1280px-Law_Chambers-011_perspective_corrected.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m3",
+    title: "This is a third meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Moses_Mabhida_Stadion_durban_aerial_view_1.jpg/1280px-Moses_Mabhida_Stadion_durban_aerial_view_1.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+];
+
 function AllMeetupsPage() {
-  return <div>All Meetups Page.</div>;
+  return (
+    <section>
+      <h1>All Meetups</h1>
+      <ul>
+        {DUMMY_DATA.map((meetup) => {
+          return <li key={meetup.id}>{meetup.title}</li>;
+        })}
+      </ul>
+    </section>
+  );
 }
 
 export default AllMeetupsPage;
