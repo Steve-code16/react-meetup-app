c2f2445faacd4044ee8a6a8f22d7a72718e50c07
Steve Molele <stevemolele@yahoo.com>
2022-04-28 00:42:02 +0200

    rendering data from database

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,51 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import MeetupList from "../components/meetups/MeetupList";
-
-// const DUMMY_DATA = [
-//   {
-//     id: "m1",
-//     title: "This is a first meetup",
-//     image:
-//       "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Moses_Mabhida_Stadion_durban_aerial_view_1.jpg/1280px-Moses_Mabhida_Stadion_durban_aerial_view_1.jpg",
-//     address: "MeetupStreet 5, 12345 Meetup City",
-//     description:
-//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-//   },
-//   {
-//     id: "m2",
-//     title: "This is a second meetup",
-//     image:
-//       "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Law_Chambers-011_perspective_corrected.jpg/1280px-Law_Chambers-011_perspective_corrected.jpg",
-//     address: "MeetupStreet 5, 12345 Meetup City",
-//     description:
-//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-//   },
-//   {
-//     id: "m3",
-//     title: "This is a third meetup",
-//     image:
-//       "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
-//     address: "MeetupStreet 5, 12345 Meetup City",
-//     description:
-//       "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
-//   },
-// ];
 
 function AllMeetupsPage() {
   const [isLoading, setIsLoading] = useState(true);
   const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-  fetch(
-    "https://react-meetup-app-bb4a6-default-rtdb.firebaseio.com/meetups.json"
-  )
-    .then((response) => {
-      response.json();
-    })
-    .then((data) => {
-      setIsLoading(false);
-      setLoadedMeetups(data);
-    });
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      "https://react-meetup-app-bb4a6-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
 
   if (isLoading) {
     return (
